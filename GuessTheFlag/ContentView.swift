5bb9967aa9f141e10628c27a3dbf9f7eef4d6b42
Thierno Diallo <tdiallo1996@gmail.com>
2024-11-17 23:38:42 -0500

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Thierno Diallo on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+        }
+    }
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Thierno Diallo on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
