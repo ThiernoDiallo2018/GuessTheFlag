ad2647efa3c05b83f806040f4946277d24a8ebc2
Thierno Diallo <tdiallo1996@gmail.com>
2024-11-19 23:45:12 -0500

    Update on UI

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
         
-        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
-            .ignoresSafeArea()
+        ZStack {
+            LinearGradient(colors: [.white, .red], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap The Flag of")
+                    Text(countries[correctAnswer])
+                }
+                
+                ForEach(0..<4) { country in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[country]) //will show random flags
+                    }
+                }
+            }
+            
         }
     }
+}
 
 #Preview {
     ContentView()
