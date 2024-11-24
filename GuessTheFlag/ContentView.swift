d9cf87df80b3ee6264430faced8b6c87db681779
Thierno Diallo <tdiallo1996@gmail.com>
2024-11-23 21:51:42 -0500

    Completed Project

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private  var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"] //Created the data to be static so it can accessed directly and not through an instance.
+    
+    
+    @State private var countries = allCountries.shuffled()
+
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    
+    @State private var score = 0
+    
+    @State private var questionCounter = 1 //Track the attempts
+
+    @State private var showingResults = false // Boolean for the alert
 
 
     
@@ -45,6 +55,7 @@ struct ContentView: View {
                             Image(countries[number]) //will show random flags
                                 .clipShape(.capsule)
                                 .shadow(radius: 10)
+                                
                         }
                     }
                 }
@@ -55,35 +66,67 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
+                Text("Attempts: \(questionCounter)")
+                    .foregroundStyle(.white)
+                    .font(.caption.bold())
                 
                 Spacer()
+                
             }.padding()
             
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
+        }
+        .alert("Game over!", isPresented: $showingResults) {
+            Button("Start Again", action: restart)
+        } message: {
+            Text("Your final score was \(score).")
         }
     }
         
         func flaggTapped(_ number: Int) {
             if number == correctAnswer {
                 scoreTitle = "Correct"
+                score += 1
             } else {
-                scoreTitle = "Wrong"
+                let needsThe = ["UK", "US"]
+                let theirAnswer = countries[number]
+
+                if needsThe.contains(theirAnswer) {
+                    scoreTitle = "Wrong! That's the flag of the \(theirAnswer)."
+                } else {
+                    scoreTitle = "Wrong! That's the flag of \(theirAnswer)."
+                }
             }
             
-            showingScore = true
+            if questionCounter == 8 {
+                showingResults = true
+            } else {
+                
+                showingScore = true
+            }
         }
     
     func askQuestion() { //Needed to change the shuffle and potential answer
-        countries.shuffled()
+        countries.remove(at: correctAnswer) // will remove the country from the array
+        countries = countries.shuffled()
         correctAnswer = Int.random(in: 0...2)
+        questionCounter += 1
     }
+    
+    func restart() {
+        questionCounter = 1
+        score = 0
+        countries = Self.allCountries
+        askQuestion()  // Just passed in the function instead of retyping all of the directions
+    }
+    
 }
 
 #Preview {
